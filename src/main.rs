9b9d60732b2af82d3bccc4a0c5f3c350ad927aad
smb123w64gb <smb123w64gb@gmail.com>
2022-10-11 14:15:19 -0700

    Start and we got file reading :)

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use clap::Parser;
+use std::path::PathBuf;
+mod mdl;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    mdl: PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("{}", &args.mdl.display());
+    let mdl = mdl::MDLFile::open(args.mdl).unwrap();
+}
---
+++ src/mdl.rs
@@ -0,0 +1,84 @@
+use binrw::{
+    BinRead, // trait for reading
+    BinReaderExt,
+    BinResult,
+};
+use modular_bitfield::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(BinRead, Copy, Clone)]
+pub struct Vec2 {
+    pub x: f32,
+    y: f32,
+}
+#[derive(BinRead, Copy, Clone, Default)]
+pub struct Vec3 {
+    pub x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[derive(BinRead, Copy, Clone)]
+pub struct RGBA {
+    pub r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+#[bitfield]
+#[derive(BinRead, Debug, PartialEq)]
+#[br(map = Self::from_bytes)]
+pub struct Flags {
+    pub norm: bool,
+    pub txtr: bool,
+    pub clrs: bool,
+    pub unk0: bool,
+    pub unk1: bool,
+    pub unk2: bool,
+    pub unk3: bool,
+    pub unk4: bool,
+}
+#[derive(BinRead, Clone)]
+#[br(import(vertex_count: u16,normals: bool))]
+pub struct Frame {
+    #[br(args{count: vertex_count as usize, inner:(normals,)})]
+    pub vertex_buff: Vec<Vertex>,
+}
+#[derive(BinRead, Copy, Clone)]
+#[br(import(normals: bool))]
+pub struct Vertex {
+    pub position: Vec3,
+    #[br(if(normals))]
+    pub normals: Option<Vec3>,
+}
+
+#[derive(BinRead)]
+#[br(little, magic = b"MDL\x00")]
+pub struct MDLFile {
+    pub flags: Flags,
+    pub idx_per_face: u8,
+
+    pub vertex_count: u16,
+    pub frame_count: u16,
+
+    #[br(if(flags.txtr()),count = vertex_count)]
+    pub texture_cords: Option<Vec<Vec2>>,
+
+    #[br(if(flags.clrs()),count = vertex_count)]
+    pub colors: Option<Vec<RGBA>>,
+
+    pub index_count: u16,
+    #[br(count = index_count as usize)]
+    pub indexs: Vec<u16>,
+
+    #[br(args{count: frame_count as usize, inner:(vertex_count,flags.norm(),)})]
+    pub ls_entry: Vec<Frame>,
+}
+
+impl MDLFile {
+    pub fn open<P: AsRef<Path>>(path: P) -> BinResult<Self> {
+        BufReader::new(std::fs::File::open(path)?).read_le()
+    }
+}
